aa3a61fa7bea9c75dd9021318b16036016830b61
aiwannafly <a.ivanov15@g.nsu.ru>
2023-04-27 14:49:07 +0700

    remove main() with testing

--- src/main/java/ru/nsu/mkc/dto/MessageMapper.java
+++ src/main/java/ru/nsu/mkc/dto/MessageMapper.java
@@ -2,33 +2,11 @@ package ru.nsu.mkc.dto;
 
 import com.evolveum.midpoint.notifications.api.transports.Message;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.NotificationMessageAttachmentType;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MessageMapper {
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
-    public static void main(String[] args) throws JsonProcessingException {
-        Message m = new Message();
-        m.setFrom("alex");
-        m.getTo().add("misha");
-        m.setBody("hello misha");
-        m.setSubject("greetings");
-        NotificationMessageAttachmentType a = new NotificationMessageAttachmentType();
-        a.contentType("number");
-        a.content(9);
-        a.fileName("num.txt");
-        m.getAttachments().add(a);
-        MessageDto messageDto = messageToDto(m);
-        byte[] data = objectMapper.writeValueAsBytes(messageDto);
-        MessageDto converted = objectMapper.readValue(new String(data, StandardCharsets.UTF_8), MessageDto.class);
-        System.out.println(converted.equals(messageDto));
-        System.out.println(converted);
-    }
 
     static public MessageDto messageToDto(Message m) {
         List<AttachmentDto> attachmentDtos = new ArrayList<>();
